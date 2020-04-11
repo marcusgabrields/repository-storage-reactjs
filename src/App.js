d6edff498a88e3c2533e591751016061163b83d1
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-04-11 17:00:56 -0300

    Add list functionality

--- src/App.js
+++ src/App.js
@@ -39,13 +39,17 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repository => (
+            <li key={ repository.id }>
+              { repository.title }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
